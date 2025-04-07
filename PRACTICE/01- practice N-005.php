1f582c50c2caf9e23669d2a7f0719b57257cc070
Abderrafie <chattabdrrafie@gmail.com>
2025-04-07 11:51:47 +0100

    for ( ; ; ) { } loop

---
+++ PRACTICE/01- practice N-005.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    // for loop :
+    for ($i = 0; $i <= 10; $i++) {
+        echo "$i - <br>";
+    }
+
+    // --
+    for ($i = 0; $i <= 10; $i++) {
+        if ($i % 2 === 0) {
+            echo "$i est un nombre pair <br>";
+        }
+    }
+
+    // --
+    for ($i = 0; $i <= 10; $i++) {
+        if ($i % 2 !== 0) {
+            echo "$i est un nombre impair <br>";
+        }
+    }
+
+    ?>
+</body>
+
+</html>
