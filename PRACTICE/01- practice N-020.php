4d22d00930f20e2ba54cdb71ea58909836f34f01
Abderrafie <chattabdrrafie@gmail.com>
2025-05-14 15:41:50 +0100

    OOP en PHP

--- PRACTICE/01- practice N-020.php
+++ PRACTICE/01- practice N-020.php
@@ -36,3 +36,51 @@ $voiture1->accelerer(50);
 $voiture1->accelerer(100);
 $voiture1->accelerer(100);
 $voiture1->details();
+
+
+// --------------------------- 
+// avec le constructeur
+
+class Voiture {
+    public $marque;
+    public $modele;
+    public $couleur;
+    public $vitessActuelle = 0;
+    public $vitessMax = 220;
+
+    // Définition du constructeur
+    public function __construct($marque, $modele, $couleur) {
+        $this->marque = $marque;
+        $this->modele = $modele;
+        $this->couleur = $couleur;
+    echo "Nouvelle voiture créée : " . $this->marque . " " . $this->modele . "\n";
+    }
+
+    public function details() {
+        echo "Marque : $this->marque \n";
+        echo "Vitesse Actuelle : $this->vitessActuelle km/h \n";
+    }
+
+    public function accelerer($acceleration) {
+
+        $this->vitessActuelle += $acceleration;
+        if ($this->vitessActuelle > $this->vitesseMax) {
+            $this->vitessActuelle = $this->vitesseMax;
+            echo "Vitesse Maximale atteinte !\n";
+        }
+        echo "La voiture accélére á $this->vitesseActuelle km/h \n";
+    }
+}
+
+// 1er objet
+$voiture1 = new Voiture("Peugeot", "208", "Bleu");
+$voiture1->details();
+$voiture1->accelerer(50);
+$voiture1->details();
+// 2eme objet
+$voiture2 = new Voiture("BMW", "Série 3", "Noire");
+$voiture2->details();
+$voiture2->accelerer(80);
+$voiture2->details();
+
+
