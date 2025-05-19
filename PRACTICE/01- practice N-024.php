c502c13d600ece340aa8f4954f8d3233879ca752
Abderrafie <chattabdrrafie@gmail.com>
2025-05-19 14:37:02 +0100

    static properties | methodes -> class

--- PRACTICE/01- practice N-024.php
+++ PRACTICE/01- practice N-024.php
@@ -1,6 +1,6 @@
 <?php
-// static properties : acceder by using keyWord => self::property  OR  static::property.
 
+// static properties : acceder by using keyWord => self::property  OR  static::property.
 class Produit {
     public static $tva = 0.2;
     public $prix;
@@ -15,5 +15,20 @@ class Produit {
 echo Produit::$tva; // Affiche : 0.2
 $p = new Produit(100);
 echo $p->prixTTC(); // Affiche : 120
+
+
+// -------------------------------------
+
+// static Methodes : acceder by using keyWord => className::Methodes
+class Outils
+{
+    public static function direBonjour($nom)
+    {
+        return "Bonjour, $nom !";
+    }
+}
+
+// Appel
+echo Outils::direBonjour("Abderrafie"); // Affiche.
 
 ?>
