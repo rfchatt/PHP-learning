77a60dfd54ea1d8f7c32b03025453a22d99fcdfe
Abderrafie <chattabdrrafie@gmail.com>
2025-05-29 16:44:00 +0100

    Add files via upload

--- DataBase/Regional2022/supprimerProduit.php
+++ DataBase/Regional2022/supprimerProduit.php
@@ -1,5 +1,15 @@
 <?php
 //connexiondb
-//req delete
-//location
-?>
+include "connexion.php";
+
+if (!isset($_GET["ref"])) {
+    header("Location: accueil.php?erreur=Produit introuvable");
+    exit();
+}
+
+$ref = $_GET["ref"];
+$stmt = $conn->prepare("DELETE FROM Produit WHERE reference = ?");
+$stmt->bindParam(1, $ref);
+$stmt->execute();
+header("Location: accueil.php?success=PrduitSupprimé");
+exit();
